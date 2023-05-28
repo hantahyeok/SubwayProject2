58e46141ab8ee8eb9f3cfc187bea02b6599425e4
hantahyeok <hantahyeok@naver.com>
2023-05-28 17:16:32 +0900

    지하철 clock par

--- app/src/main/java/com/hdk/subway/ClockActivity.java
+++ app/src/main/java/com/hdk/subway/ClockActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.location.Address;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
@@ -35,7 +37,7 @@ import java.util.List;
 
 public class ClockActivity extends AppCompatActivity {
 
-    TextView tv;
+    TextView tv, t;
     ViewPager2 pager;
     StationLineNum stationLineNum;
     List<String> list;
@@ -49,9 +51,10 @@ public class ClockActivity extends AppCompatActivity {
 
         tabbar = findViewById(R.id.tabbar);
         tv = findViewById(R.id.tv);
-        pager = findViewById(R.id.pager);
-        adapter = new MyPagerAdapter(list);
-        pager.setAdapter(adapter);
+        t = findViewById(R.id.t);
+//        pager = findViewById(R.id.pager);
+//        adapter = new MyPagerAdapter(list);
+//        pager.setAdapter(adapter);
 
         MySingleton singleton = MySingleton.getInstance();
         String stationName = singleton.getData();
@@ -59,6 +62,7 @@ public class ClockActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -66,7 +70,7 @@ public class ClockActivity extends AppCompatActivity {
             }
         });
 
-        stationNum(stationName);
+//        stationNum(stationName);
 
     }
 
@@ -86,17 +90,24 @@ public class ClockActivity extends AppCompatActivity {
 
                     Gson gson = new Gson();
                     stationLineNum = gson.fromJson(isr, StationLineNum.class);
-
-                    (stationLineNum.SearchSTNBySubwayLineInfo.row).forEach( str -> {
-                        list.add(stationLineNum.SearchSTNBySubwayLineInfo.row.toString());
-                    });
-
-//                    runOnUiThread(() -> {
-//                        pager.setAdapter(new MyPagerAdapter(list));
-//                        TabLayoutMediator mediator = new TabLayoutMediator(tabbar, pager, ((tab, position) -> )
 //
-//                    };);
+////                    (stationLineNum.SearchSTNBySubwayLineInfo.row).forEach( str -> {
+////                        list.add(stationLineNum.SearchSTNBySubwayLineInfo.row.toString());
+////                    });
 
+                    Handler handler = new Handler(Looper.getMainLooper());
+                    handler.post(() -> t.setText("새로운 텍스트"));
+//
+                    runOnUiThread(() -> {
+
+                        Toast.makeText(ClockActivity.this, "HHHH", Toast.LENGTH_SHORT).show();
+
+////                        pager.setAdapter(new MyPagerAdapter(list));
+////                        TabLayoutMediator mediator = new TabLayoutMediator(tabbar, pager, ((tab, position) -> tab.setText(list.get(position)) ));
+////
+////                        mediator.attach();
+                    });
+//
 
                 } catch (MalformedURLException e) {
                     throw new RuntimeException(e);
