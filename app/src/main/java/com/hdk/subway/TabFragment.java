bdb4946eb3842d7426aa690d4496ee2958e0c388
hantahyeok <hantahyeok@naver.com>
2023-05-31 03:37:45 +0900

    recyclerview

--- app/src/main/java/com/hdk/subway/TabFragment.java
+++ app/src/main/java/com/hdk/subway/TabFragment.java
@@ -7,12 +7,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class TabFragment extends Fragment {
+
+    TextView text;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab, container, false);
+        View v = inflater.inflate(R.layout.fragment_tab, container, false);
+
+//        text = v.findViewById(R.id.Hello);
+//        http://swopenapi.seoul.go.kr/api/subway/416678437474616837356359705349/json/realtimeStationArrival/0/1000/%EC%99%95%EC%8B%AD%EB%A6%AC
+        return v;
     }
 }
