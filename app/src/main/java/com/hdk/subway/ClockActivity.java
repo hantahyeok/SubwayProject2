1545949064f6761e97afda98185053116b3b7b06
hantahyeok <hantahyeok@naver.com>
2023-05-30 18:06:29 +0900

    지하철 호선 불러오기 100%

--- app/src/main/java/com/hdk/subway/ClockActivity.java
+++ app/src/main/java/com/hdk/subway/ClockActivity.java
@@ -29,7 +29,7 @@ import java.util.List;
 
 public class ClockActivity extends AppCompatActivity {
 
-// dkajdflsjdfksdfjlaskdfj
+// 연습 git
     TextView tv;
     ViewPager2 pager;
     List<String> list;
