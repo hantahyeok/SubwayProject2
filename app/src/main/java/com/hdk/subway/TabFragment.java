0e77ac5eb867d5d187fa56b3a0e8cd19f546a887
hantahyeok <hantahyeok@naver.com>
2023-06-06 04:16:31 +0900

    운행 종료 깜박이기

--- app/src/main/java/com/hdk/subway/TabFragment.java
+++ app/src/main/java/com/hdk/subway/TabFragment.java
@@ -218,34 +218,35 @@ public class TabFragment extends Fragment {
 
                             Handler handler = new Handler(Looper.getMainLooper());
 
-                            handler.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    if(go.get(0) == null){
-                                        AnimationSet animationSet = new AnimationSet(true);
-                                        animationSet.setInterpolator(new LinearInterpolator());
-                                        animationSet.setRepeatCount(Animation.INFINITE);
-                                        animationSet.setRepeatMode(Animation.REVERSE);
-
-                                        AlphaAnimation alphaAnimation = new AlphaAnimation(1, 0);
-                                        alphaAnimation.setDuration(1000); // 깜빡임 간격 (밀리초)
-                                        animationSet.addAnimation(alphaAnimation);
-
-                                        finish1.startAnimation(animationSet);
-                                    }else if (goline2 == null){
-                                        AnimationSet animationSet = new AnimationSet(true);
-                                        animationSet.setInterpolator(new LinearInterpolator());
-                                        animationSet.setRepeatCount(Animation.INFINITE);
-                                        animationSet.setRepeatMode(Animation.REVERSE);
-
-                                        AlphaAnimation alphaAnimation = new AlphaAnimation(1, 0);
-                                        alphaAnimation.setDuration(1000); // 깜빡임 간격 (밀리초)
-                                        animationSet.addAnimation(alphaAnimation);
-
-                                        finish2.startAnimation(animationSet);
-                                    }
-                                }
-                            });
+                            // todo : 여기서부터
+//                            handler.post(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    if(go.get(0) == null){
+//                                        AnimationSet animationSet = new AnimationSet(true);
+//                                        animationSet.setInterpolator(new LinearInterpolator());
+//                                        animationSet.setRepeatCount(Animation.INFINITE);
+//                                        animationSet.setRepeatMode(Animation.REVERSE);
+//
+//                                        AlphaAnimation alphaAnimation = new AlphaAnimation(1, 0);
+//                                        alphaAnimation.setDuration(1000); // 깜빡임 간격 (밀리초)
+//                                        animationSet.addAnimation(alphaAnimation);
+//
+//                                        finish1.startAnimation(animationSet);
+//                                    }else if (goline2 == null){
+//                                        AnimationSet animationSet = new AnimationSet(true);
+//                                        animationSet.setInterpolator(new LinearInterpolator());
+//                                        animationSet.setRepeatCount(Animation.INFINITE);
+//                                        animationSet.setRepeatMode(Animation.REVERSE);
+//
+//                                        AlphaAnimation alphaAnimation = new AlphaAnimation(1, 0);
+//                                        alphaAnimation.setDuration(1000); // 깜빡임 간격 (밀리초)
+//                                        animationSet.addAnimation(alphaAnimation);
+//
+//                                        finish2.startAnimation(animationSet);
+//                                    }
+//                                }
+//                            });
 
                             if(!trainLineNm.contains(go.get(0))){
                                 items2.add(new Item2(trainLineNm, statnNm, btrainSttus, barvlDt, bstatnNm, recptnDt, arvlMsg2, arvlMsg3, arvlCd, subwayList));
